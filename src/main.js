38d784692bb7e9ed03f95cda2e5c793599c346d3
wang <743414915@qq.com>
2023-04-03 16:10:17 +0800

    环境搭建完毕

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/base.less'
 import App from './App.vue'
+//引入cookies
+import VueCookies from 'vue-cookies'
+//引入element plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//图标
+import '@/assets/icon/iconfont.css'
+// import router from 'router/index.js'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// app.use(router);
+app.use(ElementPlus);
+app.config.globalProperties.VueCookies = VueCookies;
+
+app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // 忽略后缀名的配置选项, 添加 .vue 选项时要记得原本默认忽略的选项也要手动写入
+    // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    hmr: true,
+    port: 3004,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8083",
+        changeOrigin: true,
+        pathRewrite: {
+          "^api": "/api"
+        }
+      }
+    }
+  }
 })
